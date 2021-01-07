a4adea6f9229133fb0954bd69e26c93510af9184
twosoull <71281491+twosoull@users.noreply.github.com>
2021-01-07 20:06:23 +0900

    컨트롤러서블릿을 이용한 포워드 RequestDispatcher rd= request.getRequestDispatcher("보낼주소") rd.forward(request,response) 사용하고 담을 데이터가 있다면 request.setAttribute("별명",데이터)를 사용한다 jsp에서는 request.getAttribute("별명")으로 받을 수 있고 오브젝트로 오기 때문에 각각 맞게 변환해주면된다.

---
+++ src/com/javaex/controller/PhoneController.java
@@ -0,0 +1,73 @@
+package com.javaex.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javaex.dao.PhoneDao;
+import com.javaex.vo.PersonVo;
+
+@WebServlet("/pbc")
+public class PhoneController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		System.out.println("Controller");
+		// 리스트 출력관련
+
+		// 파라미터 action 값을 읽어서
+		String action = request.getParameter("action");
+		System.out.println(action);
+
+		// action = list ---> 리스트 출력처리
+		if ("list".equals(action)) {
+			System.out.println("리스트처리");
+			
+			PhoneDao phonedao = new PhoneDao();
+			List<PersonVo> personList = phonedao.getPersonList();
+
+			// html --> 엄청 복잡하다 -->jsp가 편하다
+			// 데이터 전달
+			request.setAttribute("pList", personList);
+
+			// jsp에 포워드 시킨다.
+			RequestDispatcher rd = request.getRequestDispatcher("./WEB-INF/list.jsp"); // jsp 파일 위치
+			rd.forward(request, response);
+
+			// action = wform -->등록폼
+		}else if("wform".equals(action)) {
+			System.out.println("등록 폼 처리");
+			RequestDispatcher rs = request.getRequestDispatcher("./WEB-INF/writeForm.jsp");
+			rs.forward(request, response);
+			
+			
+		}else if("insert".equals(action)) {
+			System.out.println("전화번호 저장");
+			//파라미터 3개 받기
+			String name = request.getParameter("name");
+			String ph = request.getParameter("hp");
+			String company = request.getParameter("company");
+			//personvo 묵고
+			PersonVo personvo = new PersonVo(name,ph,company);
+			//new dao
+			PhoneDao phonedao = new PhoneDao();
+			//dao personInsert
+			phonedao.personInsert(personvo);
+			response.sendRedirect("/phonebook2/pbc?action=list");
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// doGet(request, response);
+	}
+
+}
